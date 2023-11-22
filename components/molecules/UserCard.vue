e76385f83a451a386847501dd0e200445d07ff35
pabrul <pablo.batista@onovomercado.com.br>
2023-11-22 15:05:46 -0300

    style: 💅 Style adjustments in 'UserCard.vue' and 'UserInfo.vue'

--- components/molecules/UserCard.vue
+++ components/molecules/UserCard.vue
@@ -1,7 +1,8 @@
 <!-- components/organisms/UserCard.vue -->
 <template>
   <div
-    class="py-8 my-8 font-mono relative gap-4 flex flex-col items-center rounded-[20px] justify-center h-[150px] w-full mx-auto bg-white bg-clip-border shadow-3xl shadow-shadow-500 dark:!bg-navy-800 dark:text-white dark:!shadow-none"
+    id="card-user"
+    class="py-12 my-12 font-mono relative gap-4 flex flex-col items-center rounded-[20px] justify-center h-[150px] w-full mx-auto bg-[#212121] bg-clip-border shadow-3xl shadow-shadow-500 dark:!bg-navy-800 dark:text-white dark:!shadow-none border-solid border-indigo-200 border-[1px]"
   >
     <div class="relative flex w-full justify-center rounded-xl bg-cover">
       <Avatar
--- components/molecules/UserInfo.vue
+++ components/molecules/UserInfo.vue
@@ -1,12 +1,14 @@
 <!-- components/molecules/UserInfo.vue -->
 <template>
-  <div class="flex flex-col items-center p-4 m-4">
-    <h4 class="text-xl font-bold text-navy-700 dark:text-gray-950">
+  <div class="flex flex-col items-center p-2 m-2">
+    <h4 class="text-lg font-bold text-white dark:text-white">
+      <Icon name="🙋" />
+
       {{ userName }}
     </h4>
     <a
       :href="userUrl"
-      class="text-base font-normal text-gray-600 p-2"
+      class="text-base font-normal text-indigo-200 relative w-fit block after:block after:mt-2 after:content-[''] after:absolute after:h-[3px] after:bg-indigo-500 after:w-32 after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left p-4 hover:border-gray-700 border-transparent border-2"
       target="_blank"
     >
       <Icon name="🔗" />
