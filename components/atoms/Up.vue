9e9ed1d8b69f90739856014529c9096bd047a97c
pabrul <pablo.batista@onovomercado.com.br>
2023-11-22 15:04:07 -0300

    feat: ✨ Add 'Up' component for top page navigation

---
+++ components/atoms/Up.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="fixed m-6 p-4 right-0 bottom-0 rounded-full border-indigo-500 border-solid bg-indigo-500 cursor-pointer text-white"
+    @click="scrollToTop"
+  >
+    <span class="up-icon">👆</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
+</script>
+
+<style scoped>
+.up-icon {
+  font-size: 20px;
+}
+
+@media screen and (max-width: 768px) {
+  .up-icon {
+    font-size: 20px;
+  }
+}
+</style>
